7819c5b222328020b29e4c6374634a991f25fe44
Ali.Ghodrat <ght.ali@hotmail.com>
2016-11-08 15:46:17 +0100

    Person and Ansat initiated.

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+	
+
+    private String navn;
+
+    public Person(String navn){
+        this.navn = navn;
+    }
+    
+    public String toString(){
+        return "Navn: \t"+navn;
+    }
+
+
+}
