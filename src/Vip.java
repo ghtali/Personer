2f5d91291b3133bfd7c463aa65530ce02360c469
Ali.Ghodrat <ght.ali@hotmail.com>
2016-11-09 14:03:01 +0100

    kurser array has been defined, Vip constructure is ready.

--- src/Vip.java
+++ src/Vip.java
@@ -1,6 +1,16 @@
 
 public class Vip extends Ansat {
-
+	private int[] kurser;
+	
+	public Vip(String navn, String institut, int[] kurser){
+		super(navn, institut);
+		
+		this.kurser = new int[kurser.length];
+		for (int i=0; i< kurser.length; i++){
+			
+		}
+		
+	}
 	
 
 	
